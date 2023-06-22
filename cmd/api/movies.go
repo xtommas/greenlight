4453c01b69911062c35b7f6f3a7e69e9b7b5735b
tomas <tomascanepa1397@gmail.com>
2023-06-22 14:10:18 -0300

    3.3: created the Movies struct that represents the movies. updated the GET /v1/movies/:id endpoint to return a struct of the Movie type with dummy data encoded as JSON

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/xtommas/greenlight/internal/data"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -16,5 +19,18 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	fmt.Fprintf(w, "show the details of movie %d\n", id)
+	movie := data.Movie{
+		Id:        id,
+		CreatedAt: time.Now(),
+		Title:     "Blade Runner 2049",
+		Runtime:   163,
+		Genres:    []string{"action", "drama", "mystery", "sci-fi", "thriller"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
