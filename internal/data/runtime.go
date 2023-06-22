a5133263bb3f2fea34ac0d8e38f355a62c5b742e
tomas <tomascanepa1397@gmail.com>
2023-06-22 17:55:43 -0300

    4.4: made the runtime in the JSON request body be of the format '[runtime] mins' by implementing the UnmarshalJSON method in the Runtime type

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,9 +1,13 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 type Runtime int32
 
@@ -13,4 +17,29 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	quotedJSONValue := strconv.Quote(jsonValue)
 
 	return []byte(quotedJSONValue), nil
+}
+
+// need to use a ponter of the Runtime type to modify the receiver r, otherwise it would use just a copy
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unquotedJSONValue, " ")
+
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	// parse the string containing the number into int32
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	// conver the int32 into a Runtime and assign it to the receiver by dereferencing the pointer
+	*r = Runtime(i)
+
+	return nil
 }
