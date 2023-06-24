8d3c6cc4c1d943c3ce8a5d221dbe0d17dfa8dab9
tomas <tomascanepa1397@gmail.com>
2023-06-24 17:10:46 -0300

    6.2: added sql up and down migration files for creating the movies table and adding the constraints that reinforce the same rules that were used in the code to the db: runtime >= 0 1888 <= year <= current year 1 <= genres length <= 5

--- greelinght.sql
+++ greelinght.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE greelight;
+CREATE DATABASE greenlight;
 
 -- Create a user called greelight with a password of your choosing
-CREATE ROLE greelight WITH LOGIN PASSWORD 'password';
+CREATE ROLE greenlight WITH LOGIN PASSWORD 'password';
 -- Create extension for case-sensitive character string type
 CREATE EXTENSION IF NOT EXISTS citext;
