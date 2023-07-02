d351914da38058f653ad59be57215af432ea97c5
tomas <tomascanepa1397@gmail.com>
2023-07-02 16:16:36 -0300

    11.1: added global rate limiting using middleware to 2 requests per second or a maximum of 4 in a 'burst'

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"golang.org/x/time/rate"
 )
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
@@ -14,6 +16,22 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 				app.serverErrorResponse(w, r, fmt.Errorf("%s", err))
 			}
 		}()
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) rateLimit(next http.Handler) http.Handler {
+	// 2 requests per second with a maximum of 4 requests in a single "burst"
+	limiter := rate.NewLimiter(2, 4)
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// see if the request is permitted or not
+		if !limiter.Allow() {
+			// return 429 Too Many Requests
+			app.rateLimitExceededResponse(w, r)
+			return
+		}
 
 		next.ServeHTTP(w, r)
 	})
