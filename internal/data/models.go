508da6f80762d6901717c23b97299370d39abf72
tomas <tomascanepa1397@gmail.com>
2023-07-03 17:51:52 -0300

    13.2: added the User struct and UserModel to interface with the database. Also added it to models.go

--- internal/data/models.go
+++ internal/data/models.go
@@ -10,10 +10,12 @@ var ErrEditConflict = errors.New("edit conflict")
 
 type Models struct {
 	Movies MovieModel
+	Users  UserModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
+		Users:  UserModel{DB: db},
 	}
 }
