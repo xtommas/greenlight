a25886f3c5b534308cd4b588cc3e71931a1e3fd1
tomas <tomascanepa1397@gmail.com>
2023-07-10 17:09:37 -0300

    17.3: added the permissions model (permissions.go) and added it to the Models struct in models.go

--- internal/data/models.go
+++ internal/data/models.go
@@ -9,15 +9,17 @@ var ErrRecordNotFound = errors.New("record not found")
 var ErrEditConflict = errors.New("edit conflict")
 
 type Models struct {
-	Movies MovieModel
-	Tokens TokenModel
-	Users  UserModel
+	Movies      MovieModel
+	Permissions PermissionModel
+	Tokens      TokenModel
+	Users       UserModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
-		Movies: MovieModel{DB: db},
-		Tokens: TokenModel{DB: db},
-		Users:  UserModel{DB: db},
+		Movies:      MovieModel{DB: db},
+		Permissions: PermissionModel{DB: db},
+		Tokens:      TokenModel{DB: db},
+		Users:       UserModel{DB: db},
 	}
 }
