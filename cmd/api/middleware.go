e3337c9160949089d97c6b3df5bc33b7e6a0fa39
tomas <tomascanepa1397@gmail.com>
2023-07-12 16:36:42 -0300

    19.3: added middleware to check the number of request, the number of responses and the time it took to process them. This information is shown on the response to a GET request to the /debug/vars endpoint

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"expvar"
 	"fmt"
 	"net"
 	"net/http"
@@ -217,5 +218,26 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 		}
 
 		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) metrics(next http.Handler) http.Handler {
+	totalRequestsReceived := expvar.NewInt("total_requests_received")
+	totalResponsesSent := expvar.NewInt("total_responses_sent")
+	totalProcessingTimeMicroseconds := expvar.NewInt("total_processing_time_μs")
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		totalRequestsReceived.Add(1)
+
+		// call the next handler in the chain
+		next.ServeHTTP(w, r)
+
+		// on the way back up the middleware chain, increment the number of responses
+		totalResponsesSent.Add(1)
+
+		duration := time.Since(start).Microseconds()
+		totalProcessingTimeMicroseconds.Add(duration)
 	})
 }
