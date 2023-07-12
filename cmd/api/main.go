a48dc673266941221e17a2dfd173a9189b82a650
tomas <tomascanepa1397@gmail.com>
2023-07-12 16:28:58 -0300

    19.2: added metrics for the database, goroutines, time and version using expvar. Use the GET method on the /debug/vars endpoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"context"
 	"database/sql"
+	"expvar"
 	"flag"
 	"os"
+	"runtime"
 	"strings"
 	"sync"
 	"time"
@@ -120,6 +122,23 @@ func main() {
 	defer db.Close()
 
 	logger.PrintInfo("database connection pool established", nil)
+
+	expvar.NewString("version").Set(version)
+
+	// publish the number of active goroutines
+	expvar.Publish("goroutines", expvar.Func(func() interface{} {
+		return runtime.NumGoroutine()
+	}))
+
+	// publish the database connection pool statistics
+	expvar.Publish("database", expvar.Func(func() interface{} {
+		return db.Stats()
+	}))
+
+	// publish the current Unix timestamp
+	expvar.Publish("timestamp", expvar.Func(func() interface{} {
+		return time.Now().Unix()
+	}))
 
 	app := &application{
 		config: cfg,
